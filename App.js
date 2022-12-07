1484a5672d5dca329a5a4b0ce4cc7a9cb0b31e5f
Schomklin <kungchomklin@gmail.com>
2022-12-07 12:51:52 +0700

    Component

--- App.js
+++ App.js
@@ -6,6 +6,12 @@ import TxtInput from "./components/TxtInput";
 import CustomComponent from "./components/CustomComponent";
 import Logo from "./components/Logo";
 import LotsOfGreeting from "./components/LotsOfGreeting";
+import Count from "./components/Count";
+import TextInput1 from "./components/TextInput1";
+import AlertExample from "./components/AlertExample";
+import ImageWithTextInput from "./components/ImageWithTextInput";
+import ButtonExample from "./components/ButtonExample";
+import Touchable_Example from "./components/Touchable_Example";
 
 const App = () => {
   return (
@@ -15,9 +21,25 @@ const App = () => {
       {/* <TxtInput/> */}
       {/* <CustomComponent/> */}
       {/* <Logo/> */}
-      <LotsOfGreeting/>
+      {/* <LotsOfGreeting/> */}
+      {/* <Count num={2} title="Click"/> */}
+      {/* <TextInput1/> */}
+      {/* <AlertExample/> */}
+      {/* <ImageWithTextInput/> */}
+      {/* <ButtonExample/> */}
+      <Touchable_Example/>
+
     </View>
   );
 };
 
 export default App;
+
+/* const styles = StyleSheet.create({
+  container:{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center'
+  }
+}) */
+
