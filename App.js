3836f7dd93882ab01046cb2518c00b43cd0436fb
Schomklin <kungchomklin@gmail.com>
2022-12-14 12:57:34 +0700

    Navigator2

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import HomeScreen from "./screens/HomeScreen";
 import AboutScreen from "./screens/AboutScreen";
+import HomePost from "./screens/HomePost";
+import CreatePost from "./screens/CreatePost";
 
 
 const Stack = createNativeStackNavigator();
@@ -16,7 +18,7 @@ const App = () => {
        initialRouteName="Home"
        screenOptions={{
         headerStyle:{
-          backgroundColor :'#008b8b'
+          backgroundColor :'#66cdaa'
         },
         headerTintColor:'#ffff',
         headerTitleStyle:{
@@ -24,8 +26,10 @@ const App = () => {
         }
        }}
       >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="About" component={AboutScreen} />
+{/*     <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} /> */}
+        <Stack.Screen name="HomePost" component={HomePost} />
+        <Stack.Screen name="CreatPost" component={CreatePost} /> 
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,11 @@
 import { View, Text, Button } from "react-native";
 import React from "react";
+import {Ionicons,FontAwesome } from '@expo/vector-icons'
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Ionicons name='home' size={40} color='#008b8b'/>
       <Text>Home Screen</Text>
       <Button
         title="เกี่ยวกับเรา"
