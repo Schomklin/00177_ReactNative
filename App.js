5d64d94059b845129f0c6dd96724a38d8117b0b3
Schomklin <kungchomklin@gmail.com>
2023-01-11 12:11:16 +0700

    "DrawerContent"

--- App.js
+++ App.js
@@ -1,30 +1,78 @@
-import { View, Text,Button } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 
-import { createDrawerNavigator} from '@react-navigation/drawer';
-import { NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem
+} from '@react-navigation/drawer';
 
-function HomeScreen (){
-  return(
-    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+function Feed({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Feed Screen</Text>
       <Button
-       title='Go to Notifications'      
+        title="Open Drawer"
+        onPress={() => navigation.openDrawer()}
       />
-
+      <Button
+        title="Toggle Drawer"
+        onPress={() => navigation.toggleDrawer()}
+      />
     </View>
-
   );
 }
+
+function Article() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Article Screen</Text>
+    </View>
+  );
+}
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Close Drawer"
+        onPress={() => props.navigation.closeDrawer() }
+      />
+       <DrawerItem
+        label="Toggle Drawer"
+        onPress={() => props.navigation.toggleDrawer() }
+      />
+    </DrawerContentScrollView>
+  );
+}
+
 const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: '#b0e0e6',
+          width: 240
+        }
+      }}
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Article" component={Article} />
+    </Drawer.Navigator>
+  )
+}
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
-       useLegacyImplementation    
-      >
-        <Drawer.Screen name="Home" component={HomeScreen}/>
-      </Drawer.Navigator>
+      <MyDrawer />
     </NavigationContainer>
   )
 }
