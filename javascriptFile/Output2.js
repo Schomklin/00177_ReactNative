2ea9a8b32f6f9c062fef2d9120dced69de49c913
Schomklin <kungchomklin@gmail.com>
2023-06-23 23:44:13 +0700

    "JavaScript2"

---
+++ javascriptFile/Output2.js
@@ -0,0 +1,14 @@
+let user = {
+    name: "Apichai",
+    age: 28
+};
+function hello() {
+    console.log("Hello World!");
+}
+
+console.log(user);
+console.log([10, 20, 30, 40, 50]);
+hello();
+
+
+
