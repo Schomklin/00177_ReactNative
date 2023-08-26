6457e94a94e8fb1b896d4249486bfe820b79c37a
Schomklin <kungchomklin@gmail.com>
2023-08-26 11:51:25 +0700

    "callAPIProduct"

--- App.js
+++ App.js
@@ -2,23 +2,25 @@ import { View, Text ,StyleSheet} from 'react-native'
 import React from 'react'
 import FlastListAPI from './components/FlastListAPI'
 import News from './components/News'
+import ProductScreen from './components/ProductScreen'
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <View>
       {/* <FlastListAPI/> */}
-      <News/>
+      <ProductScreen/>
+      {/* <News/> */}
     </View>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container:{
     flex:1,
     justifyContent:'center',
     alignItems:'center'
 
-  }
-})
+  } 
+}) */
